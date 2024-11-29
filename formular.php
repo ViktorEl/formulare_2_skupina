bdad5e4bc4f19845e303423adf5c1ec167f8d145
Your Name <you@example.com>
2024-11-29 09:16:37 +0100

    vytvorenie formulára

--- formular.php
+++ formular.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
+
+    <form action="" name="registracia" method="post">
+        <input type="text" name="meno" placeholder="Zadajte meno"> <br>
+        <input type="password" name="heslo" placeholder="Zadajte heslo"> <br>
+        <input type="submit" name="tlacidlo" value="Odoslať"> <br>
+    </form>
+
+
+
+    <?php
+
+
+    ?>
+
     
 </body>
 </html>
