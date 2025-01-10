40a9a2efe7f8c60e0bd51a83589aaf2ea9b26a50
Your Name <you@example.com>
2025-01-10 08:30:13 +0100

    Vytvorenie formulára typu radio

--- index.php
+++ index.php
@@ -2,20 +2,11 @@
 <html lang="en">
 <?php include "hlavicka.php" ?>
 <body>
-
+    <h2>Vyberte si kurz</h2>
     <form action="formular.php" method="post">
-        <label>Zadajte prvé číslo</label>
-        <input type="number" name="cislo1" min="0" max="1000000"> <br>
-        <label>Zadajte druhé číslo</label>
-        <input type="number" name="cislo2" min="0" max="1000000"> <br>
-        <label>Vyber operátor</label>
-        <select name="operator">
-            <option value="1">--</option>
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select> <br>
+        <input type="radio" name="pohlavie" value="muz"> <label>muž</label> <br>
+        <input type="radio" name="pohlavie" value="zena"> <label>žena</label> <br>
+        <input type="radio" name="pohlavie" value="ine"> <label>nechcem uviesť</label> <br>
         <input type="submit" name="tlacidlo" value="Odoslať">
     </form>
 
