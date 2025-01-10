20c029d0341e80d9470853909aacc952e08aa274
Your Name <you@example.com>
2025-01-10 09:43:50 +0100

    skytie formulara

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include "hlavicka.php" ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
 <body>
-    <h2>Vyberte si kurz</h2>
-    <form action="formular.php" method="post">
-        <input type="checkbox" name="kurz[]" value="1"> <label>PHP</label> <br>
-        <input type="checkbox" name="kurz[]" value="2"> <label>Python</label> <br>
-        <input type="checkbox" name="kurz[]" value="3"> <label>Java</label> <br>
-        <input type="submit" name="tlacidlo" value="Odoslať">
+    <h1>Učíme sa skryť formulár</h1>
+
+    <?php
+        $stav = false;
+        if(isset($_POST["tlacidlo"])) {
+            $stav = true;
+            if(isset($_POST["meno"]) && isset($_POST["priezvisko"]) && !empty($_POST["meno"]) && !empty($_POST["priezvisko"]) ) {
+                $meno = $_POST["meno"];
+                $priezvisko = $_POST["priezvisko"];
+                echo "vase meno je: ".$meno." ".$priezvisko;
+            }
+        }
+    ?>
+
+
+    <form action="index.php" method="post" id="<?php if($stav == true) echo "skrytie"; ?>">
+        <input type="text" name="meno" placeholder="Zadajde meno" required> <br>
+        <input type="text" name="priezvisko" placeholder="Zadajte priezvisko" required> <br>
+        <input type="submit" name="tlacidlo" value="Odoslať"> <br>
     </form>
 
+
+
+
+    
 </body>
 </html>
